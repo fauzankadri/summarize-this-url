407753498cb556a65a6e879a4fb99a87f97161b6
Fauzan <f_auzan@hotmail.com>
2018-04-10 22:16:41 -0400

    serve favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const request = require('request');
 const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
 
 var bodyParser = require('body-parser');
 
